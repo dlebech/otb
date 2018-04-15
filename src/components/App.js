89c939d89698a92d2f4281fd7d99f48f3cbf0886
David Volquartz Lebech <david@lebech.info>
2018-04-15 19:18:15 +0200

    Add TreeMap chart for transactions
    This commit adds a new TreeMap chart that shows transactions by their
    size. Also included is a simple menu for slightly easier navigation
    between upload and charts.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Intro from './Intro';
+import Menu from './Menu';
 import TransactionUpload from './TransactionUpload';
 import Chart from './Chart';
 
 const App = props => {
   return (
     <Router>
-      <div className="container mt-4">
-        <Route exact path="/" component={Intro} />
-        <Route exact path="/upload" component={TransactionUpload} />
-        <Route exact path="/chart" component={Chart} />
-      </div>
+      <React.Fragment>
+        <Route path="/*" component={Menu} />
+        <div className="container mt-4">
+          <Route exact path="/" component={Intro} />
+          <Route exact path="/upload" component={TransactionUpload} />
+          <Route exact path="/chart" component={Chart} />
+        </div>
+      </React.Fragment>
     </Router>
   );
 }
