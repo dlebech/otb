539aafe19a97dfb1b42b8551a48960694e524451
David Volquartz Lebech <david@lebech.info>
2018-04-15 19:22:20 +0200

    Add TreeMap chart for transactions (#4)
    This commit adds a new TreeMap chart that shows transactions by their
    size. Also included is a simple menu for slightly easier navigation
    between upload and charts.

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,30 +1,79 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { ResponsiveContainer, LineChart, Line, Tooltip, XAxis, YAxis } from 'recharts';
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  Treemap,
+  Tooltip,
+  XAxis,
+  YAxis
+} from 'recharts';
 import { nest } from 'd3-collection';
 import { sum } from 'd3-array';
+
+// Bootstrap colors
+// TODO: Should probably find a better way to store these.
+const colors = {
+  dark: '#343a40',
+  white: '#ffffff'
+};
+
+const AmountPerDay = props => {
+  const data = nest()
+    .key(d => d.date)
+    .rollup(a => sum(a, d => d.amount))
+    .entries(props.transactions.data)
+    .sort((a, b) => a.key.localeCompare(b.key))
+
+  return (
+    <ResponsiveContainer>
+      <LineChart data={data}>
+        <XAxis dataKey="key" />
+        <YAxis />
+        <Tooltip />
+        <Line
+          type="monotone"
+          dataKey="value"
+          stroke={colors.dark}
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+const AmountPerCategory = props => {
+  const data = nest()
+    .key(d => d.description)
+    .rollup(a => Math.abs(sum(a, d => d.amount)))
+    .entries(props.transactions.data)
+    .sort((a, b) => b.value - a.value);
+
+  return (
+    <ResponsiveContainer>
+      <Treemap
+        data={data}
+        nameKey="key"
+        dataKey="value"
+        stroke={colors.white}
+        fill={colors.dark}
+      >
+        <Tooltip />
+      </Treemap>
+    </ResponsiveContainer>
+  )
+};
 
 const Chart = props => {
   if (props.transactions.data.length === 0) return null;
 
-  let data = props.transactions.data
-    .sort((a, b) => a.date.localeCompare(b.date));
-  data = nest()
-    .key(d => d.date)
-    .rollup(a => sum(a, d => d.amount))
-    .entries(data);
-
   return (
     <div className="row justify-content-center">
       <div className="col-6 chart">
-        <ResponsiveContainer>
-          <LineChart data={data}>
-            <XAxis dataKey="key" />
-            <YAxis />
-            <Tooltip />
-            <Line type="monotone" dataKey="value" />
-          </LineChart>
-        </ResponsiveContainer>
+        <AmountPerDay {...props} />
+      </div>
+      <div className="col-6 chart">
+        <AmountPerCategory {...props} />
       </div>
     </div>
   );
---
+++ src/components/Menu.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Menu = props => {
+  // Do not return the menu on the front page.
+  if (props.match.url === '/') return null;
+
+  const active = url => {
+    return props.match.url === url ? ' active' : '';
+  }
+
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/">Off The Books</Link>
+        <div className="navbar-collapse">
+          <ul className="navbar-nav">
+            <li className={`nav-item${active('/upload')}`}>
+              <Link className="nav-link" to="/upload">Upload</Link>
+            </li>
+            <li className={`nav-item${active('/chart')}`}>
+              <Link className="nav-link" to="/chart">Charts</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Menu;
