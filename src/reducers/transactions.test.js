ded9e1ca2c4c2ac0b679902b032cd035be4b2c5e
David Volquartz Lebech <david@lebech.info>
2018-06-03 21:11:52 +0200

    Fix test

--- src/reducers/transactions.test.js
+++ src/reducers/transactions.test.js
@@ -399,5 +399,5 @@ it('should delete a transaction', () => {
 it('should create test data', () => {
   const state = reducers({}, actions.createTestData());
   expect(state.transactions.data.length).toBeGreaterThan(100);
-  expect(state.transactions.data[1].total).toBeGreaterThan(state.transactions.data[0].total);
+  expect(state.transactions.data[0].total).toBeGreaterThan(state.transactions.data[1].total);
 });
