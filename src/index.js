8c8bd14706e8f7ef7c218a81f203b2c8efc329e6
David Volquartz Lebech <david@lebech.info>
2018-06-03 14:12:41 +0200

    Ensure font-awesome icons during testing

--- src/index.js
+++ src/index.js
@@ -5,47 +5,12 @@ import { PersistGate } from 'redux-persist/integration/react';
 import './css/index.css';
 import App from './components/App';
 import configureStore from './configureStore';
+import configureFa from './configureFa';
 import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.css'; // Makes sure bootstrap is always available.
 
-// Build the fontawesome library
-import fontawesome from '@fortawesome/fontawesome';
-import faUpload from '@fortawesome/fontawesome-free-solid/faUpload';
-import faChartBar from '@fortawesome/fontawesome-free-solid/faChartBar';
-import faTable from '@fortawesome/fontawesome-free-solid/faTable';
-import faDatabase from '@fortawesome/fontawesome-free-solid/faDatabase';
-import faQuestionCircle from '@fortawesome/fontawesome-free-solid/faQuestionCircle';
-import faEdit from '@fortawesome/fontawesome-free-solid/faEdit';
-import faLightbulb from '@fortawesome/fontawesome-free-solid/faLightbulb';
-import faDownload from '@fortawesome/fontawesome-free-solid/faDownload';
-import faThumbsDown from '@fortawesome/fontawesome-free-solid/faThumbsDown';
-import faThumbsUp from '@fortawesome/fontawesome-free-solid/faThumbsUp';
-import faTrashAlt from '@fortawesome/fontawesome-free-solid/faTrashAlt';
-import faPlus from '@fortawesome/fontawesome-free-solid/faPlus';
-import faBan from '@fortawesome/fontawesome-free-solid/faBan';
-import faSort from '@fortawesome/fontawesome-free-solid/faSort';
-import faSortUp from '@fortawesome/fontawesome-free-solid/faSortUp';
-import faSortDown from '@fortawesome/fontawesome-free-solid/faSortDown';
-fontawesome.library.add(
-  faUpload,
-  faChartBar,
-  faTable,
-  faDatabase,
-  faQuestionCircle,
-  faEdit,
-  faLightbulb,
-  faDownload,
-  faThumbsUp,
-  faThumbsDown,
-  faTrashAlt,
-  faPlus,
-  faBan,
-  faSort,
-  faSortUp,
-  faSortDown
-);
-
+configureFa();
 const { store, persistor } = configureStore({});
 
 ReactDOM.render(
