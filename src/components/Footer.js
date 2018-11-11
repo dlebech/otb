3df07e72b8873fece01bfb5acb03abfb43d8dd70
David Volquartz Lebech <david@lebech.info>
2018-11-11 20:28:59 +0100

    Add version to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { version } from '../../package.json';
 
-const Footer = props => {
+const Footer = () => {
   return (
-    <React.Fragment>
+    <>
       <hr />
       <div className="container">
         <div className="row justify-content-center my-3">
           <div className="col-auto">
-            <Link to="/privacy">Privacy Policy</Link> / <a href="https://github.com/dlebech/off-the-books">Source Code</a>
-          </div>
-          <div className="col-auto">
+            <Link to="/privacy">Privacy Policy</Link> / <a href="https://github.com/dlebech/off-the-books">Source Code</a> / Version { version }
           </div>
         </div>
       </div>
-    </React.Fragment>
+    </>
   );
 };
 
