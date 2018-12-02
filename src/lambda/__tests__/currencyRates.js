f6b6255e54dd2ba8244d95b17b8fe1f6254eb7df
David Volquartz Lebech <david@lebech.info>
2018-12-02 09:34:47 +0100

    Fill missing currency rates on weekends
    Closes #67 and fixes #80

--- src/lambda/__tests__/currencyRates.js
+++ src/lambda/__tests__/currencyRates.js
@@ -26,13 +26,29 @@ describe('currencyRates', () => {
   it('should return daily rates', async () => {
     const res = await handler();
     const rates = {
+      '2018-09-17': {
+        USD: 1.1691,
+        JPY: 130.77
+      },
       '2018-09-14': {
-        USD: 1.1689,
-        JPY: 130.75
+        USD: 1.1690,
+        JPY: 130.76
       },
       '2018-09-13': {
         USD: 1.1689,
         JPY: 130.75
+      },
+      // The 15-16 didn't have currencies but for completeness, the last known
+      // currency rates are returned.
+      '2018-09-15': {
+        USD: 1.1690,
+        JPY: 130.76,
+        refDate: '2018-09-14'
+      },
+      '2018-09-16': {
+        USD: 1.1690,
+        JPY: 130.76,
+        refDate: '2018-09-14'
       }
     };
 
@@ -49,11 +65,22 @@ describe('currencyRates', () => {
       }
     });
     const rates = {
+      '2018-09-17': {
+        USD: 1.1691
+      },
       '2018-09-14': {
-        USD: 1.1689
+        USD: 1.1690
       },
       '2018-09-13': {
         USD: 1.1689
+      },
+      '2018-09-15': {
+        USD: 1.1690,
+        refDate: '2018-09-14'
+      },
+      '2018-09-16': {
+        USD: 1.1690,
+        refDate: '2018-09-14'
       }
     };
 
